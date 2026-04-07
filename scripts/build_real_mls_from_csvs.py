1287eeb9e13360b0af8b65af88e49a990f813009
boimenredd <slurpcute1@gmail.com>
2026-04-07 15:43:05 -0400

    Delete scripts directory

--- scripts/build_real_mls_from_csvs.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-"""Build js/real-mls-data-v18.js from the uploaded MLS player CSV exports."""
-print("This build already bundles a CSV-derived dataset. Re-run the container-side generator if you want to rebuild it.")
